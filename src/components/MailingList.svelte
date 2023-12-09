51df0f084246b7255e5857b049ad49adca4792f8
Alejandro Estrella <aqealejandro@gmail.com>
2023-12-08 23:35:55 -0800

    added mailing list and removed banner

---
+++ src/components/MailingList.svelte
@@ -0,0 +1,3 @@
+<div class="h-[1000px] filter dark:hue-rotate-180 dark:invert-[0.91]">
+    <iframe id="mailinglist" src="https://docs.google.com/forms/d/e/1FAIpQLSdhbMDj9qu1JL9Y18V_oPlCdEvZWh8vDBHOvb2siCtmitbOLQ/viewform?embedded=true" title="Mailing List Signup" frameborder=0 class="rounded-lg w-full" style="height: 100% !important;">Loading…</iframe>
+</div>
